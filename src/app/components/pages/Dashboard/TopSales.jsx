7aacf43d5d9944294390543167208a7bc752064c
Justina Sokolova <Justinasal@yandex.ru>
2023-04-07 22:49:33 +0300

    feat: top sales API integration

--- src/app/components/pages/Dashboard/TopSales.jsx
+++ src/app/components/pages/Dashboard/TopSales.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import topSalesNftService from "../../../services/topSalesNft.service";
 
 const TopSales = () => {
+  const [topSalesData, settopSalesData] = useState();
+  const [isLoading, setLoading] = useState(true);
+
+  const monthTime = 30;
+  const weekTime = 7;
+  const dayTime = 1;
+
+  async function getSalesList(value) {
+    try {
+      const content = await topSalesNftService.get(value);
+      settopSalesData(content);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getSalesList(monthTime);
+  }, []);
+
+  console.log(topSalesData);
+
   return <div>TopSales</div>;
 };
 export default TopSales;
